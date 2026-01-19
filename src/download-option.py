d5c9e3830af567b793fbe48051e64e7d97bdccef
textyang <textyang@gmail.com>
2026-01-18 20:40:07 -0500

    remove symbol from script

--- src/download-option.py
+++ src/download-option.py
@@ -2,6 +2,7 @@ import os
 import datetime
 import pytz
 import re
+from pathlib import Path
 
 if __name__ == "__main__":
 
@@ -13,8 +14,13 @@ if __name__ == "__main__":
     path=f'data/{date}'
     cmd=f'rm -rf {path}; mkdir -p {path}'
     os.system(cmd)
+
+    current_path = str(Path.cwd())
+    m=re.search('\/([^\/]+)\-option',current_path)
+    if(m!=None):
+        symbol=m.group(1)
     
-    cmd=f'python3 src/yahoo-option.py QQQ {path}'
+    cmd=f'python3 src/yahoo-option.py {symbol} {path}'
     os.system(cmd)
 
     cmd=f'git add {path}/*.csv'
